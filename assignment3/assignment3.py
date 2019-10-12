78a131a9903a905532cb7840e7d91a44599ddd44
cw02048 <cw02048@naver.com>
2019-10-13 00:19:31 +0900

    과제 3 완료
    ㅅㅅㅅ

--- assignment3/assignment3.py
+++ assignment3/assignment3.py
@@ -8,7 +8,19 @@ Created on Thu Oct 10 14:46:34 2019
 # 과제 3
 
 
-circle_mask
-upper_half_mask, logical_end
+import imageio
+from skimage import io
+import matplotlib.pyplot as plt
+import numpy as np
 
-np.logical_end()
+photo_data = io.imread('sd-3layers.jpg')
+photo_data.shape
+radius = 3725 / 2
+x, y = np.mgrid[0:3725, 0:4797]
+circle = ((radius - x)**2 + (4797/2 - y)**2)**0.5 > radius
+photo_data[circle] = 255
+
+black_white = np.logical_and(circle, x >= 1862 )
+photo_data[black_white] = 0
+
+plt.imshow(photo_data)
